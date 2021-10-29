36c2f091f93ce72850dc4f88edf5c009fb8d32e3
JordanMaduro <jordanmaduro@gmail.com>
2021-10-29 16:49:37 +0000

    Add Antd, change prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Button } from "antd";
 
 function App() {
-  return <div className="bg-white">Hello Vite</div>
+  return (
+    <div className="bg-white">
+      Hello Vite
+      <Button type="primary">Button</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
