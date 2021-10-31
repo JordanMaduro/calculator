9704b7ad23b95a3f1e98fe7f667d9de6b87ab068
Jordan Maduro <jordanmaduro@gmail.com>
2021-10-31 14:23:35 +0100

    feat(display): add subText to display

--- src/components/display/display.tsx
+++ src/components/display/display.tsx
@@ -4,14 +4,20 @@ import { useDisplay } from "./hooks";
 
 export interface DisplayProps {
   text?: string;
+  subText?: string;
 }
 
-const Display: FC<DisplayProps> = ({ text }) => {
-  const { value } = useDisplay({ text });
+const Display: FC<DisplayProps> = ({ text, subText }) => {
+  const { value, subValue } = useDisplay({ text, subText });
 
   return (
-    <div className="text-right text-2xl">
-      {value === "" ? <>&nbsp;</> : value}
+    <div>
+      <div className="text-right text-xl">
+        {subValue === "" ? <>&nbsp;</> : subValue}
+      </div>
+      <div className="text-right text-2xl">
+        {value === "" ? <>&nbsp;</> : value}
+      </div>
     </div>
   );
 };
