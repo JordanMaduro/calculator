f084ef20d5c3dbe41c92abcc13a82804056724cf
Jordan Maduro <jordanmaduro@gmail.com>
2021-11-01 00:19:43 +0100

    Fix lint issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Calculator } from "./components/calculator";
 
 function App() {
   return (
-    <div className="flex bg-gray-100 h-screen justify-center pt-8">
+    <div className="flex justify-center pt-8 h-screen bg-gray-100">
       <div className="w-64">
         <Calculator />
       </div>
