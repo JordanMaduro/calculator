b8e875d7a51abedd25d0326aeff62c6edbccf682
Jordan Maduro <jordanmaduro@gmail.com>
2021-11-01 17:47:41 +0100

    feat(app): add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Calculator } from "./components/calculator";
 
 function App() {
   return (
-    <div className="flex justify-center pt-8 h-screen bg-gray-100">
-      <div className="w-64">
+    <div className="flex justify-center md:pt-8 h-screen bg-gray-100">
+      <div className="w-full md:w-64">
         <Calculator />
       </div>
     </div>
